70f219bd4395816016fe970ade164d3e66f08d4e
Jerry Ong <jerry.ong@creditworks.co.nz>
2023-06-15 13:47:36 +1200

    Add project files.

---
+++ ToyRobotSimulation.Test/ToyRobotTest.cs
@@ -0,0 +1,16 @@
+namespace ToyRobotSimulation.Test
+{
+    public class ToyRobotTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
